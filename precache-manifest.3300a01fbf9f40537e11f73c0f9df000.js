b01a8be0929b8434d9ec2526b51e1ae116c37b6f
Vladyslav Zuiev <vladyslavzuiev@Vladyslavs-MacBook-Pro.local>
2020-05-23 16:38:10 +0200

    Updates

---
+++ precache-manifest.3300a01fbf9f40537e11f73c0f9df000.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9862b7dae23959a846e7132dec6c4e55",
+    "url": "/hr.landing-page/index.html"
+  },
+  {
+    "revision": "13eea630c74a17209bb9",
+    "url": "/hr.landing-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bf4b40ef3de856bcfd1",
+    "url": "/hr.landing-page/static/js/2.e6d05517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hr.landing-page/static/js/2.e6d05517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13eea630c74a17209bb9",
+    "url": "/hr.landing-page/static/js/main.c909d88f.chunk.js"
+  },
+  {
+    "revision": "d2a021ec74bcf9d436e4",
+    "url": "/hr.landing-page/static/js/runtime-main.798eccda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hr.landing-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
